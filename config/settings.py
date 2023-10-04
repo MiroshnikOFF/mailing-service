aea3f3606479c136b75bafd43540cc9426b22635
miroshnikoff <taxi83080@gmail.com>
2023-10-04 14:04:32 +0300

    delete users

--- config/settings.py
+++ config/settings.py
@@ -136,6 +136,10 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# AUTH_USER_MODEL = 'users.User'
+# LOGOUT_REDIRECT_URL = '/'
+# LOGIN_REDIRECT_URL = '/'
+# LOGIN_URL = '/users/'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
