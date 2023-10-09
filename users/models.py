7e7a703a0cbe67b50f8a2bea72a9a5e73f1dc81b
miroshnikoff <taxi83080@gmail.com>
2023-10-09 23:50:22 +0300

    refactor users

--- users/models.py
+++ users/models.py
@@ -11,7 +11,24 @@ class User(AbstractUser):
 
     phone = models.CharField(max_length=35, verbose_name='Номер телефона', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
+    key = models.CharField(max_length=12, verbose_name='Ключ для верификации почты', **NULLABLE)
+    is_verified = models.BooleanField(default=False, verbose_name='Почта верифицирована')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} ({self.email})'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        permissions = [
+            (
+                'set_is_active_user',
+                'Can active user'
+            )
+        ]
+
+
 
