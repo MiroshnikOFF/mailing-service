a969d29fc79aec811c74f43289b9d3aa8df99386
miroshnikoff <taxi83080@gmail.com>
2023-10-12 01:25:10 +0300

    refactor users

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from users.apps import UsersConfig
-from users.views import LoginView, LogoutView, RegisterView, UserUpdateView, email_verification, password_recovery, \
-    UserListView, user_toggle_activity
+from users.views import LoginView, LogoutView, RegisterView, UserProfile, email_verification, password_recovery, \
+    UserListView, user_toggle_activity, UserDitailView, UserDeleteView, UserUpdateView
 
 app_name = UsersConfig.name
 
@@ -10,9 +11,12 @@ urlpatterns = [
     path('', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', UserUpdateView.as_view(), name='profile'),
+    path('user/<int:pk>/', UserDitailView.as_view(), name='user'),
+    path('profile/', UserProfile.as_view(), name='profile'),
+    path('update/<int:pk>', UserUpdateView.as_view(), name='user_update'),
+    path('delete/<int:pk>/', UserDeleteView.as_view(), name='user_delete'),
     path('verification/', email_verification, name='verification'),
     path('recovery/', password_recovery, name='recovery'),
-    path('users/', UserListView.as_view(), name='users_list'),
+    path('users/', cache_page(60)(UserListView.as_view()), name='users_list'),
     path('user/activity/<int:pk>/', user_toggle_activity, name='user_activity'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,14 +1,14 @@
 import random
 
 from django.contrib.auth.decorators import permission_required, login_required
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import Group
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView, UpdateView, ListView
+from django.views.generic import CreateView, UpdateView, ListView, DetailView, DeleteView
 
 from config import settings
 from users.forms import UserRegisterForm, UserForm
@@ -16,12 +16,20 @@ from users.models import User
 
 
 class RegisterView(CreateView):
+    """Контроллер для регистрации пользователя"""
+
     model = User
     form_class = UserRegisterForm
     success_url = reverse_lazy('users:login')
     template_name = 'users/register.html'
 
     def form_valid(self, form):
+        """
+        Создает пароль для подтверждения email, сохраняет его в объекте пользователя
+        и отправляет на email указанный при регистрации.
+        Сохраняет пользователя в группе user.
+        """
+
         user = form.save()
         key = ''.join([str(random.randint(0, 9)) for _ in range(12)])
         user.key = key
@@ -37,9 +45,16 @@ class RegisterView(CreateView):
 
 
 class LoginView(BaseLoginView):
+    """Контроллер для входа пользователя"""
+
     template_name = 'users/login.html'
 
     def get_success_url(self):
+        """
+        Если email пользователя верифицирован, перенаправляет на главную страницу,
+        иначе перенаправляет на страницу верификации email.
+        """
+
         user = self.request.user
         if user.is_verified:
             return reverse('service:home')
@@ -47,20 +62,67 @@ class LoginView(BaseLoginView):
 
 
 class LogoutView(BaseLogoutView):
+    """Контроллер для выхода пользователя"""
+
     pass
 
 
-class UserUpdateView(LoginRequiredMixin, UpdateView):
+class UserDitailView(LoginRequiredMixin, DetailView):
+    """Контроллер для вывода информации о пользователе"""
+
     model = User
-    success_url = reverse_lazy('users:profile')
+
+
+class UserProfile(LoginRequiredMixin, UpdateView):
+    """Контроллер для редактирования своего профиля текущим пользователем"""
+
+    model = User
     form_class = UserForm
 
     def get_object(self, queryset=None):
+        """Возвращает объект текущего пользователя"""
+
         return self.request.user
+
+    def get_success_url(self):
+        """Возвращает на страницу вывода информации о пользователе"""
+
+        return reverse('users:user', args=[self.object.id])
+
+
+class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """Контроллер для редактирования пользователя"""
+
+    model = User
+    form_class = UserForm
+    success_url = reverse_lazy('users:users_list')
+
+    def test_func(self):
+        """Задает доступ к редактированию пользователя только для суперпользователя"""
+
+        return self.request.user.is_superuser
+
+
+class UserDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """Контроллер для удаления пользователя"""
+
+    model = User
+    success_url = reverse_lazy('users:users_list')
+
+    def test_func(self):
+        """Задает доступ к удалению пользователя только для суперпользователя"""
+
+        return self.request.user.is_superuser
 
 
 @login_required
 def email_verification(request):
+    """
+    Верифицирует email пользователя. Если введенный пользователем код не верный, перенаправляет на
+    страницу где сообщается что код не верный. Если код верный, перенаправляет на страницу где сообщается
+    что код верный и устанавливает True в поле 'is_verified' пользователя.
+    """
+
     if request.method == 'POST':
         input_key = request.POST.get('key')
         try:
@@ -73,8 +135,13 @@ def email_verification(request):
     return render(request, 'users/verification.html')
 
 
-@login_required
 def password_recovery(request):
+    """
+    Формирует новый пароль и пытается выслать его на email введенный пользователем.
+    Если пользователь с таким email не зарегистрирован, перенаправляет на страницу где об этом сообщается.
+    Если пользователь найден, высылает на его email новый пароль.
+    """
+
     if request.method == 'POST':
         new_password = ''.join([str(random.randint(0, 9)) for _ in range(12)])
         input_email = request.POST.get('email')
@@ -94,14 +161,22 @@ def password_recovery(request):
     return render(request, 'users/password_recovery.html')
 
 
-class UserListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
-    permission_required = 'users.view_user'
+class UserListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    """Контроллер для вывода списка пользователей"""
+
     model = User
+
+    def test_func(self):
+        """Задает доступ к списку пользователей только для персонала"""
+
+        return self.request.user.is_staff
 
 
 @login_required
 @permission_required('users.set_is_active_user')
 def user_toggle_activity(request, pk):
+    """Блокирует/активирует пользователя"""
+
     user = User.objects.get(pk=pk)
     if user.is_active:
         user.is_active = False
