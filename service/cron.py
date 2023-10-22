eccba032e78850ddd26d3dd87c441612b645f31b
miroshnikoff <taxi83080@gmail.com>
2023-10-22 22:55:11 +0300

    refactor cron.py

--- service/cron.py
+++ service/cron.py
@@ -9,7 +9,7 @@ NOW_WITHOUT_SECONDS = datetime.now().time().replace(second=0, microsecond=0)
 def send_message() -> None:
     """
     Получает из БД все рассылки предназначенные для отправки и если текущая дата совпадает с датой в поле next_run,
-    то запускает рассылки по временны указанном в поле start. Cron-функция срабатывает каждую минуту.
+    то запускает рассылки по времени указанном в поле start. Cron-функция срабатывает каждую минуту.
     """
     mailings = Mailing.objects.filter(status='Создана').filter(next_run=datetime.now().date())
     for mailing in mailings:
